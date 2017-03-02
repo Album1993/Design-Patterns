0b5ad51ac77c63f0fab0e4a6ab5f4708e151e957
张一鸣 <2283942163@qq.com>
2017-03-02 10:34:00 +0800

    Initial Commit

---
+++ sheijimoshi/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sheijimoshi
+//
+//  Created by 张一鸣 on 2017/3/2.
+//  Copyright © 2017年 张一鸣. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
