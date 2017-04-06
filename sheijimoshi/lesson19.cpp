b793033cbb946afe6bf58963792b7e7ace9da1ee
zhangyiming <2283942163@qq.com>
2017-04-06 20:35:25 +0800

    责任链模式

---
+++ sheijimoshi/lesson19.cpp
@@ -0,0 +1,9 @@
+//
+//  lesson19.cpp
+//  sheijimoshi
+//
+//  Created by 张一鸣 on 2017/4/6.
+//  Copyright © 2017年 张一鸣. All rights reserved.
+//
+
+#include "lesson19.hpp"
---
+++ sheijimoshi/lesson19.hpp
@@ -0,0 +1,84 @@
+//
+//  lesson19.hpp
+//  sheijimoshi
+//
+//  Created by 张一鸣 on 2017/4/6.
+//  Copyright © 2017年 张一鸣. All rights reserved.
+//
+
+#ifndef lesson19_hpp
+#define lesson19_hpp
+
+#include <iostream>
+
+using namespace std;
+
+
+class Request {
+public:
+    string requestType;
+    string requestContent;
+    int number;
+
+};
+
+class Manager {
+protected:
+    Manager * superior;
+    string name ;
+    
+    
+public:
+    Manager(string name):name(name){}
+    virtual ~Manager(){};
+    void SetSuperior(Manager * superior) {
+        this->superior = superior;
+    }
+    virtual void RequestApplication(Request * request){}
+    
+};
+
+
+class CommonManager:public Manager {
+    
+public:
+    CommonManager(string name):Manager(name){}
+    void RequestApplication(Request * request){
+        if (request->requestType == "请假" && request->number <= 2) {
+            cout<<name<<request->requestContent<<request->number<<endl;
+        } else {
+            if (superior != nullptr) {
+                superior->RequestApplication(request);
+            }
+        }
+    }
+};
+
+class Majordomo:public Manager {
+    
+    
+public:
+    Majordomo(string name):Manager(name){}
+    void RequestApplication(Request * request){
+        if (request->requestType == "请假" && request->number <= 5) {
+            cout<<name<<request->requestContent<<request->number<<endl;
+        } else {
+            if (superior != nullptr) {
+                superior->RequestApplication(request);
+            }
+        }
+    }
+};
+
+class GeneralManager:public Manager {
+    
+    
+public:
+    GeneralManager(string name):Manager(name){}
+    void RequestApplication(Request * request){
+        
+    }
+};
+
+
+#endif /* lesson19_hpp */
